ac03ba002b967168b5ad6a182de1a295e831274c
rivalDevYP <yathavanparamesh1998@hotmail.com>
2019-01-21 11:58:48 -0500

    workshop 2

---
+++ workshop2/inLab/workshop2/workshop2/Timekeeper.cpp
@@ -0,0 +1,19 @@
+/*
+	program: Timekeeper.cpp
+	programmer: yathavan, parameshwaran
+	date: January 21, 2019
+	description: workshop 2 in lab portion
+*/
+
+#include <iostream>
+#include <string>
+#include <chrono>
+#include "Timekeeper.h"
+#include "Text.h"
+
+using namespace std;
+
+namespace sict
+{
+
+}
--- workshop2/inLab/workshop2/workshop2/Timekeeper.h
+++ workshop2/inLab/workshop2/workshop2/Timekeeper.h
@@ -1,1 +1,24 @@
-#pragma once
+/*
+	program: Timekeeper.h
+	programmer: yathavan, parameshwaran
+	date: January 21, 2019
+	description: workshop 2 in lab portion
+*/
+
+#ifndef SICT_TIMEKEEPER_H
+#define SICT_TIMEKEEPER_H
+
+#include <iostream>
+
+namespace sict
+{
+	class Timekeeper
+	{
+		const int max = 10;
+
+	public:
+
+	};
+}
+
+#endif //SICT_TIMEKEEPER_H

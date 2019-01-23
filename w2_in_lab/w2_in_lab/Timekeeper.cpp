ee075b2a092be2b7f5c1c04d3f563b3525c8cb06
rivalDevYP <yathavanparamesh1998@hotmail.com>
2019-01-23 00:44:11 -0500

    w2 in lab addition

---
+++ w2_in_lab/w2_in_lab/Timekeeper.cpp
@@ -0,0 +1,40 @@
+/*
+	program: Timekeeper.cpp
+	programmer: yathavan, parameshwaran
+	date: January 21, 2019
+	description: workshop 2 in lab portion
+*/
+
+#include <iostream>
+#include <string>
+#include <chrono>
+#include "Timekeeper.h"
+#include "Text.h"
+
+using namespace std;
+
+namespace sict
+{
+	Timekeeper::Timekeeper()
+	{
+
+	}
+
+	void Timekeeper::start() const
+	{
+	}
+
+	void Timekeeper::stop() const
+	{
+	}
+
+	void Timekeeper::recordEvent() const
+	{
+	}
+
+	void Timekeeper::report() const
+	{
+	}
+
+	
+}

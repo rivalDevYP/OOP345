16de07e14324e5f1662231836a4963627ea2f198
rivalDevYP <yathavanparamesh1998@hotmail.com>
2019-01-23 23:49:17 -0500

    Text.cpp update
    updated the one arg constructor with stuff to do fileIO and read the number of lines... also i have 2 c++ reference books on my crotch and i can't feel my legs
    
    send help and a new pair of nuts

--- visualstudio_solutions/w2_in_lab/w2_in_lab/Text.cpp
+++ visualstudio_solutions/w2_in_lab/w2_in_lab/Text.cpp
@@ -6,6 +6,7 @@
 */
 
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <chrono>
 #include "Timekeeper.h"
@@ -21,16 +22,40 @@ namespace sict
 	}
 	Text::Text(const char * incomingStr)
 	{
+		std::ifstream fileptr(incomingStr);
+		int lineCount = 0;
+		std::string bufferOne;
+		if (fileptr.is_open())
+		{
+			while (!fileptr.eofbit) //while eofbit(error flag for end of file) has not been met yet
+			{
+				std::getline(fileptr,bufferOne,'\n');
+				bufferOne.clear();
+				lineCount++;
+			}
+
+		}
 	}
 	Text::Text(const Text & incomingTextObj)
 	{
+		*this = incomingTextObj; //copy constructor called when new object created from existing object, make life easier by just calling copy assignment operator on the object that you are recieving while keeping (*this) as the left hand operand ('this' returns the address of the current object) so basically you are saying copy rhs over to lhs, in other words, copy incomingObj over to the current object
 	}
 	Text & Text::operator=(const Text & incomingObj)
 	{
-		// TODO: insert return statement here
+		if (this != &incomingObj)
+		{
+			numOfStringsCurrentlyStored = incomingObj.numOfStringsCurrentlyStored;
+			delete[]strArray;
+			strArray = nullptr;
+			strArray = new std::string[incomingObj.strArray->length];
+			strArray = incomingObj.strArray;
+		}
+		return *this;
 	}
 	Text::~Text()
 	{
+		delete[]strArray;
+		strArray = nullptr;
 	}
 	size_t Text::size() const
 	{
--- visualstudio_solutions/w2_in_lab/w2_in_lab/Text.h
+++ visualstudio_solutions/w2_in_lab/w2_in_lab/Text.h
@@ -14,6 +14,7 @@ namespace sict
 {
 	class Text
 	{
+		int numOfStringsCurrentlyStored; //class keeps track of the number of strings currently stored 
 		std::string* strArray;
 	public:
 		Text();

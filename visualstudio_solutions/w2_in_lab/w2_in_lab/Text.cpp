02571275b234f875e1051a0ac56fd33e4c89bc57
rivalDevYP <yathavanparamesh1998@hotmail.com>
2019-01-24 21:54:24 -0500

    i don't even know...

--- visualstudio_solutions/w2_in_lab/w2_in_lab/Text.cpp
+++ visualstudio_solutions/w2_in_lab/w2_in_lab/Text.cpp
@@ -27,44 +27,42 @@ namespace sict
 		std::string bufferOne;
 		if (fileptr.is_open())
 		{
-			while (!fileptr.eof() && fileptr.good()) //while eofbit(error flag for end of file) has not been met yet
+			while (!fileptr.eof() && fileptr.good()) 
 			{
-				std::getline(fileptr, bufferOne, '\n'); //extract line into bufferOne
-				bufferOne.clear(); //clear out the bufferOne (we're just counting lines at this point)
-				lineCount++; //increment line count by one
+				std::getline(fileptr, bufferOne, '\n'); 
+				bufferOne.clear(); 
+				lineCount++; 
 			}
-			//at this point, you've read down the file counting the lines
-			//you are now at the end of the file, you now gotta climb back up :)
-			if (fileptr.eof()) //eof returns true if end of file has been reached
+			
+			
+			if (fileptr.eof()) 
 			{
-				//first, clear the eof flag
 				fileptr.clear();
-				//next, go to the beginning of the file
 				fileptr.seekg(0, fileptr.beg);
 			}
-			//you now have the number of lines in the file, now allocate dynamic memory for the string pointer
+			
 			strArray = new std::string[lineCount];
 			int index = 0;
-			while (!fileptr.eof() && fileptr.good()) //while eofbit(error flag for end of file) has not been met yet
+			while (!fileptr.eof() && fileptr.good()) 
 			{
-				std::getline(fileptr, strArray[index], '\n'); //reading line and storing in array of string objects
-				index++; //incrementing the value that holds the current position of string array
+				std::getline(fileptr, strArray[index], '\n'); 
+				index++; 
 			}
 		}
 	}
-	Text::Text(const Text & incomingTextObj) //copy constructor
+	Text::Text(const Text & incomingTextObj) 
 	{
-		*this = incomingTextObj; //copy constructor called when new object created from existing object, make life easier by just calling copy assignment operator on the object that you are recieving while keeping (*this) as the left hand operand ('this' returns the address of the current object) so basically you are saying copy rhs over to lhs, in other words, copy incomingObj over to the current object
+		*this = incomingTextObj; 
 	}
-	Text & Text::operator=(const Text & incomingObj) //copy assignment operator
+	Text & Text::operator=(const Text & incomingObj) 
 	{
-		if (this != &incomingObj) //check for self-assignment (you don't wanna copy over to the same object)
+		if (this != &incomingObj) 
 		{
-			this->numOfStringsCurrentlyStored = incomingObj.numOfStringsCurrentlyStored; //shallow copy over non resource variables
-			delete[]this->strArray; //deallocate any previously allocated memory
-			this->strArray = nullptr; //good practice?
-			this->strArray = new std::string[incomingObj.strArray->length()]; //allocate new memory for the size of the incomingObject's pointer(s)
-			this->strArray = incomingObj.strArray; //copy over the resource data from the source object to the freshly allocated pointer of the current object
+			this->numOfStringsCurrentlyStored = incomingObj.numOfStringsCurrentlyStored; 
+			delete[]this->strArray; 
+			this->strArray = nullptr; 
+			this->strArray = new std::string[incomingObj.strArray->length()]; 
+			this->strArray = incomingObj.strArray; 
 		}
 		return *this;
 	}
--- visualstudio_solutions/w2_in_lab/w2_in_lab/Timekeeper.cpp
+++ visualstudio_solutions/w2_in_lab/w2_in_lab/Timekeeper.cpp
@@ -18,26 +18,10 @@ using namespace std;
 
 namespace sict
 {
-	/*Timekeeper::record::record()
-	  {
-	  this->message = nullptr;
-	  this->timeUnits = nullptr;
-	  }*/
-
 	Timekeeper::Timekeeper()
 	{
 		objectCreationCount++;
 	}
-
-	/*std::chrono::steady_clock::time_point Timekeeper::start()
-	  {
-	  return std::chrono::steady_clock::time_point();
-	  }
-
-	  std::chrono::steady_clock::time_point Timekeeper::end()
-	  {
-	  return std::chrono::steady_clock::time_point();
-	  }*/
 
 	void sict::Timekeeper::start()
 	{
@@ -49,29 +33,16 @@ namespace sict
 		endTime = std::chrono::steady_clock::now();
 	}
 
-	void Timekeeper::recordEvent(const char* incomingStr) 
+	void Timekeeper::recordEvent(const char* incomingStr)
 	{
-		/*
-		   a modifier that receives the address of a C-style null terminated string that holds the description of the event � this function copies the address of the description into the next time record, calculates the duration of the event and copies it into the next time record, copies the address of the literal string containing a description of the units of time into the next time records, and increments the instance variable that identifies the next empty time record.
-
-		   - recieves c-style string that contains description of the event (incomingStr)
-		   - copy address of incomingStr into next-time record
-		   - calculate duration of event (hint: subtract timepoint objects) and insert that into the next-time record as well
-		   - and then increment the instance variable that identifies the next-time record
-		   */
-
 		std::strncpy(recordObj[numOfRecords].message, incomingStr, (strlen(incomingStr) + 1));
 		auto milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime);
 		recordObj[numOfRecords].durationTimePiece = milliseconds;
-
 		numOfRecords++;
 	}
 
 	void Timekeeper::report(std::ostream& ostrObj) const
 	{
-		/*
-		   a query that receives a reference to an std::ostream object, inserts the title �Execution Times:� into the object and inserts each of the time records stored in the Timekeeper object 
-		   */
 		for (int index = 0; index < numOfRecords; index++)
 		{
 			ostrObj << recordObj[index].message;

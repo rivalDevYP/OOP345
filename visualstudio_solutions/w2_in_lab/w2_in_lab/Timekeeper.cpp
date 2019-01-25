51708224f1af713257fa44ed9bc5c6c4977f1d9a
rivalDevYP <yathavanparamesh1998@hotmail.com>
2019-01-25 12:46:29 -0500

    fuck

---
+++ visualstudio_solutions/w2_in_lab/w2_in_lab/Timekeeper.cpp
@@ -0,0 +1,37 @@
+/*
+program: Timekeeper.cpp
+programmer: yathavan, parameshwaran
+date: January 21, 2019
+description: workshop 2 in lab portion
+*/
+
+#include <iostream>
+#include <chrono>
+#include <string>
+#include <cstring>
+
+#include "Timekeeper.h"
+
+using namespace std;
+
+namespace sict
+{
+    Timekeeper::Timekeeper()
+    {
+        
+    }
+
+    Timekeeper::~Timekeeper()
+    {
+
+    }
+
+    void Timekeeper::start() 
+    {
+
+    }
+
+
+
+
+}

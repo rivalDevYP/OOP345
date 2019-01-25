86a8120e342e63cacdc54e8efc696c99c80b7dd4
rivalDevYP <yathavanparamesh1998@hotmail.com>
2019-01-25 12:41:50 -0500

    update timekeeper

---
+++ visualstudio_solutions/w2_in_lab/w2_in_lab/Timekeeper.h
@@ -0,0 +1,34 @@
+/*
+program: Timekeeper.h
+programmer: yathavan, parameshwaran
+date: January 21, 2019
+description: workshop 2 in lab portion
+*/
+
+#include <iostream>
+#include <chrono>
+
+namespace sict
+{
+    const int maxNumOfRecords = 10;
+    class Timekeeper
+    {
+        int numOfRecords;
+        std::chrono::steady_clock::time_point startTime;
+        std::chrono::steady_clock::time_point endTime;
+
+        typedef struct 
+        {
+            char* messageString;
+            char* unitsOfTime;
+            std::chrono::steady_clock::duration durationTime;
+        } record[maxNumOfRecords];
+
+    public:
+        Timekeeper();
+        ~Timekeeper();
+        void start();
+        void stop();
+        void recordEvent(const char* incomingStr);
+    };
+}

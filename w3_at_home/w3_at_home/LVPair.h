df24a5b95225cca3afd8f19d642c202ffc88bad4
rivalDevYP <yathavanparamesh1998@hotmail.com>
2019-02-05 11:49:28 -0500

    original syntaxing

--- w3_at_home/w3_at_home/LVPair.h
+++ w3_at_home/w3_at_home/LVPair.h
@@ -87,8 +87,10 @@ namespace sict
 		//template specialization that initializes the starting value for an LVPair<std::string, std::string > type to an empty string
 		
 		/* professor provided */
+
 		template<> //template specialization 
 		std::string SummableLVPair<std::string, std::string>::initial = std::string(""); //returnType className<templateParams>::noClue = std::string("");
+
 		/* professor provided */
 
 	};

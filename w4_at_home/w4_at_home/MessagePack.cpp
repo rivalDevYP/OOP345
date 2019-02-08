0c63a77ab0b1eed448ba194f8593b4f2ba840c86
rivalDevYP <yathavanparamesh1998@hotmail.com>
2019-02-08 15:02:36 -0500

    added MessagePack and Notifications modules to w4_at_home

--- w4_at_home/w4_at_home/MessagePack.cpp
+++ w4_at_home/w4_at_home/MessagePack.cpp
@@ -8,5 +8,38 @@
 
 namespace sict
 {
-
+	MessagePack::MessagePack()
+	{
+	}
+	MessagePack::MessagePack(Message & incomingObj, size_t sizeOfArray)
+	{
+	}
+	MessagePack::~MessagePack()
+	{
+	}
+	MessagePack::MessagePack(const MessagePack & incomingObj)
+	{
+	}
+	MessagePack & MessagePack::operator=(const MessagePack & incomingObj)
+	{
+		// TODO: insert return statement here
+	}
+	MessagePack::MessagePack(MessagePack && incomingObj)
+	{
+	}
+	MessagePack & MessagePack::operator=(MessagePack && incomingObj)
+	{
+		// TODO: insert return statement here
+	}
+	void MessagePack::display(std::ostream & os) const
+	{
+	}
+	size_t MessagePack::size() const
+	{
+		return size_t();
+	}
+	std::ostream operator<<(std::ostream & os, const MessagePack & incomingObj)
+	{
+		return std::ostream();
+	}
 }
--- w4_at_home/w4_at_home/MessagePack.h
+++ w4_at_home/w4_at_home/MessagePack.h
@@ -3,18 +3,31 @@
 // professor: Chris, Szalwinski
 // project: workshop 4
 
-#ifndef SICT_MESSAGE_H
-#define SICT_MESSAGE_H
+#ifndef SICT_MESSAGEPACK_H
+#define SICT_MESSAGEPACK_H
 
 #include <iostream>
+
+#include "Message.h"
 
 namespace sict
 {
 	class MessagePack
 	{
+		const Message* msg;
+		const Message** msg;
 	public:
-
+		MessagePack();
+		MessagePack(Message& incomingObj, size_t sizeOfArray);
+		~MessagePack();
+		MessagePack(const MessagePack& incomingObj);
+		MessagePack& operator=(const MessagePack& incomingObj);
+		MessagePack(MessagePack&& incomingObj);
+		MessagePack& operator=(MessagePack&& incomingObj);
+		void display(std::ostream& os) const;
+		size_t size() const;
 	};
+	std::ostream operator<<(std::ostream& os, const MessagePack& incomingObj);
 }
 
-#endif //SICT_MESSAGE_H
+#endif //SICT_MESSAGEPACK_H

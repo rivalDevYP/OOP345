98cde84da0bd8759d73d259818384195f1a621bf
rivalDevYP <yathavanparamesh1998@gmail.com>
2019-03-14 13:56:16 -0400

    readRecord function was completely incorrect...

--- w6_at_home/w6_at_home/Product.cpp
+++ w6_at_home/w6_at_home/Product.cpp
@@ -41,59 +41,7 @@ namespace sict
 	//readRecord, reads records from incoming file object
 	iProduct* readRecord(std::ifstream& file)
 	{
-		static int callCount{ 0 };
-		long proNum{ 0 };
-		double price{ 0.0f };
-		char taxableStatus{ '\0' };
-		std::string temppstr;
-		static std::string* tempStr;
-		int index{ 0 };
-
-		if (file.is_open() && callCount == 0)
-		{
-			size_t counter{ 0 };
-
-			if (file.eof())
-			{
-				file.clear();
-				file.seekg(0, file.beg);
-			}
-
-			while (!file.eof() && file.good())
-			{
-				std::getline(file, temppstr, '\n');
-				temppstr.clear();
-				counter++;
-			}
-
-			if (file.eof())
-			{
-				file.clear();
-				file.seekg(0, file.beg);
-			}
-
-			tempStr = new std::string[counter];
-
-			while (!file.eof() && file.good())
-			{
-				std::getline(file, tempStr[index++], '\n');
-			}
-		}
-		if (tempStr[callCount].length() < 13)
-		{
-			proNum = std::stol(tempStr[callCount].substr(0, tempStr[callCount].find(' ')));
-			price = std::stod(tempStr[callCount].substr(tempStr[callCount].find(' '), tempStr[callCount].length()));
-			callCount++;
-			return new Product(proNum, price);
-		}
-		else
-		{
-			proNum = std::stol(tempStr[callCount].substr(0, tempStr[callCount].find(' ')));
-			price = std::stod(tempStr[callCount].substr(tempStr[callCount].find(' '), tempStr[callCount].length()));
-			taxableStatus = tempStr[callCount].at(12);
-			callCount++;
-			return new TaxableProduct(proNum, price, taxableStatus);
-		}
+		
 	}
 
 	//ostream overload, prints iProduct object to output

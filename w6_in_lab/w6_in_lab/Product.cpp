72af8783f80b08a054bd4491e63f7b06dba890d0
rivalDevYP <yathavanparamesh1998@hotmail.com>
2019-03-19 21:33:26 -0400

    workshop 6 in lab addition through visual studio

---
+++ w6_in_lab/w6_in_lab/Product.cpp
@@ -0,0 +1,88 @@
+//	program		:	Product.cpp
+//	programmer	:	Yathavan, Parameshwaran
+//	date		:	March 5, 2019
+//	professor	:	Chris, Szalwinski
+
+#include "Product.h"
+
+extern int FW;
+
+namespace sict
+{
+	Product::Product() {}
+
+	//2 arg. constructor, initialized object w/ incoming params
+	Product::Product(size_t incomingProductNum, double incomingPrice)
+	{
+		this->myProductNum = incomingProductNum;
+		this->myPrice = incomingPrice;
+	}
+
+	//price query, returns price
+	double Product::price() const
+	{
+		return this->myPrice;
+	}
+
+	//display query, prints product description to output
+	void Product::display(std::ostream & os) const
+	{
+		int fieldWidth = FW;
+
+		os << this->myProductNum
+			<< std::setw(fieldWidth)
+			<< this->myPrice
+			<< " " << std::endl;
+	}
+
+	//readRecord, reads records from incoming file object
+	iProduct* readRecord(std::ifstream& file)
+	{
+        std::string temp;
+		size_t proNum{0};
+		double priceVar{0.0f};
+
+		if (!file.eof())
+		{
+			std::getline(file, temp, '\n');
+			proNum = std::stoi(temp.substr(0, temp.find_first_of(' ')));
+			priceVar = std::stod(temp.substr(temp.find_first_of(' '), temp.length()));
+			temp.clear();
+			return new Product(proNum, priceVar);
+		}
+		else
+		{
+			throw("end of file met!");
+		}		
+	}
+
+	//ostream overload, prints iProduct object to output
+	std::ostream& operator<<(std::ostream & os, const iProduct & p)
+	{
+		p.display(os);
+		return os;
+	}
+	// TaxableProduct::TaxableProduct(unsigned int incomingProNum, double incomingPrice, char incomingTaxType) : Product(incomingProNum, incomingPrice)
+	// {
+	// 	isTaxable = incomingTaxType;
+	// }
+	// double TaxableProduct::price() const
+	// {
+	// 	if (isTaxable == 'H')
+	// 	{
+	// 		double tempPrice = this->price();
+	// 		double tax = (tempPrice / 100) * HST;
+	// 		return tempPrice + tax;
+	// 	}
+	// 	else if (isTaxable == 'P')
+	// 	{
+	// 		double tempPrice = this->price();
+	// 		double tax = (tempPrice / 100) * PST;
+	// 		return tempPrice + tax;
+	// 	}
+	// }
+	// void TaxableProduct::display(std::ostream & os) const
+	// {
+	// 	os << isTaxable;
+	// }
+}
---
+++ w6_in_lab/w6_in_lab/Sale.h
@@ -0,0 +1,22 @@
+//	program		:	Sale.h
+//	programmer	:	Yathavan, Parameshwaran
+//	date		:	March 5, 2019
+//	professor	:	Chris, Szalwinski
+
+#ifndef SICT_SALE_H
+#define SICT_SALE_H
+
+#include "iProduct.h"
+
+namespace sict
+{
+	class Sale
+	{
+		std::vector <iProduct*> proObj;
+	public:
+		Sale(const char* incomingFileName);
+		void display(std::ostream& os) const;
+	};
+}
+
+#endif //SICT_SALE_H

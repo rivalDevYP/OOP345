6a938193917f4e7357d079001bfe39a8656706e7
rivalDevYP <yathavanparamesh1998@gmail.com>
2019-03-22 13:20:01 -0400

    workshop 8 in_lab addition

---
+++ w8_in_lab/Utilities.cpp
@@ -0,0 +1,29 @@
+// Workshop 8 - Smart Pointers
+// Utilities.cpp
+// Chris Szalwinski from Cornel Barna
+// 2019/03/17
+
+#include <memory>
+#include "List.h"
+#include "Element.h"
+#include "Utilities.h"
+
+using namespace std;
+
+namespace sict {
+	List<Product> mergeRaw(const List<Description>& desc, const List<Price>& price) {
+		List<Product> priceList;
+		// TODO: Add your code here to build a list of products
+		//         using raw pointers
+
+
+
+
+
+
+
+
+
+		return priceList;
+	}
+}

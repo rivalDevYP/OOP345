2fbfa9bf72db013ae9548c944dcc83adb01711e6
Yathavan Parameshwaran <43413546+rivalDevYP@users.noreply.github.com>
2019-03-22 15:15:27 -0400

    Add files via upload
    DataTable.h updated

--- w7_at_home/w7_at_home/DataTable.h
+++ w7_at_home/w7_at_home/DataTable.h
@@ -22,115 +22,141 @@ extern int ND;
 namespace sict
 {
 	template <typename T>
-		class DataTable
+	class DataTable
+	{
+		std::deque<T> xValue;
+		std::deque<T> yValue;
+
+		T accumulatedXValues{};
+		T accumulatedYValues{};
+		T xMean{};
+		T yMean{};
+		T XstandardDeviation{};
+		T YstandardDeviation{};
+		T XYcorrelation{};
+		T XYslope{};
+		T Yintercept{};
+		T median{};
+
+		int numOfRecords{ 0 };
+
+	public:
+		//single argument constructor, initializes object with values from incoming file
+		DataTable(std::ifstream &incomingFileObject)
 		{
-			std::deque<T> xValue;
-			std::deque<T> yValue;
+			xValue.clear();
+			yValue.clear();
+			std::string temp;
 
-			T accumulatedXValues{};
-			T accumulatedYValues{};
-			T xMean{};
-			T yMean{};
-			T XstandardDeviation{};
-			T YstandardDeviation{};
-			T XYcorrelation{};
-			T XYslope{};
-			T Yintercept{};
-			T median{};
+			T tempNum{ 0 };
+			T tempPrice{ 0.0f };
 
-			int numOfRecords{0};
-
-		public:
-			//single argument constructor, initializes object with values from incoming file
-			DataTable(std::ifstream &incomingFileObject)
+			if (incomingFileObject.is_open())
 			{
-				xValue.clear();
-				yValue.clear();
-				std::string temp;
-
-				T tempNum{0};
-				T tempPrice{0.0f};
-
-				if (incomingFileObject.is_open())
+				while (!incomingFileObject.eof() && incomingFileObject.good())
 				{
-					while (!incomingFileObject.eof() && incomingFileObject.good())
-					{
-						std::getline(incomingFileObject, temp, '\n');
+					std::getline(incomingFileObject, temp, '\n');
 
-						int positionFirstSpace = temp.find_first_of(' ');
+					size_t positionFirstSpace = temp.find_first_of(' ');
 
-						tempPrice = std::stof(temp.substr(0, positionFirstSpace));
-						tempNum = std::stof(temp.substr(positionFirstSpace, temp.length()));
+					tempPrice = std::stof(temp.substr(0, positionFirstSpace));
+					tempNum = std::stof(temp.substr(positionFirstSpace, temp.length()));
 
-						xValue.push_back(tempPrice);
-						yValue.push_back(tempNum);
+					xValue.push_back(tempPrice);
+					yValue.push_back(tempNum);
 
-						temp.clear();
-						tempNum = 0;
-						tempPrice = 0.0f;
+					temp.clear();
+					tempNum = 0;
+					tempPrice = 0.0f;
 
-						numOfRecords++;
-					}
+					numOfRecords++;
 				}
-
-				xMean = (std::accumulate(xValue.begin(), xValue.end(), 0.0)) / xValue.size();
-				yMean = (std::accumulate(yValue.begin(), yValue.end(), 0)) / yValue.size();
-
-				accumulatedXValues = std::accumulate(xValue.begin(), yValue.end(), 0.0);
-				accumulatedYValues = std::accumulate(yValue.begin(), yValue.end(), 0.0);
-
-				XstandardDeviation = std::sqrt ( ( accumulatedXValues * ( std::for_each ( xValue.begin(), xValue.end(), [=](T incomingVal) { return ((incomingVal - xMean) * (incomingVal - xMean)); } ) ) ) /(xValue.size() - 1) );
 			}
 
-			//display query, prints contents of deque to display
-			void displayData(std::ostream &os) const
+			xMean = (std::accumulate(xValue.begin(), xValue.end(), 0.0)) / xValue.size();
+			yMean = (std::accumulate(yValue.begin(), yValue.end(), 0)) / yValue.size();
+
+			accumulatedXValues = std::accumulate(xValue.begin(), yValue.end(), 0.0);
+			accumulatedYValues = std::accumulate(yValue.begin(), yValue.end(), 0.0);
+
+			XstandardDeviation = std::sqrt(
+				(accumulatedXValues * (std::for_each(
+					xValue.begin(), xValue.end(), [=](T incomingVal)
 			{
-				int myFieldWidth = FW;
-				int myPrecision = ND;
+				return ((incomingVal - xMean) * (incomingVal - xMean));
+			}))) / (xValue.size() - 1));
+		}
 
-				if (numOfRecords == 0)
-				{
-					throw("ERROR: container is empty!");
-				}
-				else
-				{
-					os << "Data Values"
-						<< "\n------------" << std::endl;
+		//display query, prints contents of deque to display
+		void displayData(std::ostream &os) const
+		{
+			int myFieldWidth = FW;
+			int myPrecision = ND;
 
+			if (numOfRecords == 0)
+			{
+				throw("ERROR: container is empty!");
+			}
+			else
+			{
+				os << "Data Values"
+					<< "\n------------" << std::endl;
+
+				os << std::right << std::fixed
+					<< std::setw(myFieldWidth)
+					<< "x"
+					<< std::setw(myFieldWidth)
+					<< " y"
+					<< std::endl;
+
+				for (int index = 0; index < numOfRecords; index++)
+				{
 					os << std::right << std::fixed
 						<< std::setw(myFieldWidth)
-						<< "x"
+						<< std::setprecision(myPrecision) << xValue.at(index)
 						<< std::setw(myFieldWidth)
-						<< " y"
+						<< std::setprecision(myPrecision) << yValue.at(index)
 						<< std::endl;
-
-					for (int index = 0; index < numOfRecords; index++)
-					{
-						os << std::right << std::fixed
-							<< std::setw(myFieldWidth)
-							<< std::setprecision(myPrecision) << xValue.at(index)
-							<< std::setw(myFieldWidth)
-							<< std::setprecision(myPrecision) << yValue.at(index)
-							<< std::endl;
-					}
 				}
 			}
+		}
 
-			//display query, displays the calculated statistics of the object's values
-			void displayStatistics(std::ostream &os) const
-			{
-				int myPrecision = ND;
+		//display query, displays the calculated statistics of the object's values
+		void displayStatistics(std::ostream &os) const
+		{
+			int myPrecision = ND;
 
-				os << "\nStatistics"
-					<< "\n----------" << std::endl;
+			os << "\nStatistics"
+				<< "\n----------" << std::endl;
 
-				 os << std::left << std::setw(12) << "  y mean" << std::left << std::setw(3) << "=" << std::right << std::setw(7) << std::setprecision(myPrecision) << yMean << std::endl;
-				// os << std::left << std::setw(12) << "  y sigma" << std::left << std::setw(3) << "=" << std::right << std::setw(7) << std::setprecision(myPrecision) << YstandardDeviation << std::endl;
-				// os << std::left << std::setw(12) << "  y median" << std::left << std::setw(3) << "=" << std::right << std::setw(7) << std::fixed << std::setprecision(4) << median << std::endl;
-				// os << std::left << std::setw(12) << "  slope" << std::left << std::setw(3) << "=" << std::right << std::setw(7) << std::setprecision(myPrecision) << XYslope << std::endl;
-				// os << std::left << std::setw(12) << "  intercept" << std::left << std::setw(3) << "=" << std::right << std::setw(7) << std::setprecision(myPrecision) << Yintercept << std::endl;
-			}
-		};
+			os << std::left << std::setw(12) << "  y mean" << std::left << std::setw(3) << "=" << std::right << std::setw(7) << std::setprecision(myPrecision) << yMean << std::endl;
+			// os << std::left << std::setw(12) << "  y sigma" << std::left << std::setw(3) << "=" << std::right << std::setw(7) << std::setprecision(myPrecision) << YstandardDeviation << std::endl;
+			// os << std::left << std::setw(12) << "  y median" << std::left << std::setw(3) << "=" << std::right << std::setw(7) << std::fixed << std::setprecision(4) << median << std::endl;
+			// os << std::left << std::setw(12) << "  slope" << std::left << std::setw(3) << "=" << std::right << std::setw(7) << std::setprecision(myPrecision) << XYslope << std::endl;
+			// os << std::left << std::setw(12) << "  intercept" << std::left << std::setw(3) << "=" << std::right << std::setw(7) << std::setprecision(myPrecision) << Yintercept << std::endl;
+		}
+	};
 } // namespace sict
 
 #endif //SICT_DATA_TABLE_H
+
+/*
+
+binary for_each
+sigma(i) * (Zi-ZMean)^2
+sum=0.0
+for_each
+{
+	sum+=(Zi-Zmean)*(Zi-ZMean)
+}
+
+
+accumulate
+( *,*,0,
+	[=](int sum, int x)
+	{
+		return sum+(s-Zmean)*(x-Zmean);
+	};
+);
+
+*/

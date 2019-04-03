25b239f74fd0ca8112f92248a569732c8429f050
rivalDevYP <yathavanparamesh1998@gmail.com>
2019-04-03 18:38:48 -0400

    workshop 8 at home initialization

---
+++ w8_at_home/Utilities.cpp
@@ -0,0 +1,57 @@
+// Workshop 8 - Smart Pointers
+// Utilities.cpp
+// Chris Szalwinski from Cornel Barna
+// 2019/03/17
+
+#include <memory>
+#include "List.h"
+#include "Element.h"
+#include "Utilities.h"
+
+using namespace std;
+
+namespace sict {
+	List<Product> mergeRaw(const List<Description>& desc, const List<Price>& price) {
+		List<Product> priceList;
+		// TODO: Add your code here to build a list of products
+		//         using raw pointers
+
+		std::string errorMsg = "*** Negative prices are invalid ***";
+		for (size_t index = 0; index < desc.size(); index++)
+		{
+			for (size_t tempNum = 0; tempNum < price.size(); tempNum++)
+			{
+				if (desc[index].code == price[tempNum].code)
+				{
+					Product *newObj = new Product(desc[index].desc, price[tempNum].price);
+
+					try
+					{
+						newObj->validate();
+						priceList.operator+=(newObj);
+						delete newObj;
+					}
+					catch (...)
+					{
+						throw(errorMsg);
+					}
+				}
+			}
+		}
+
+		return priceList;
+	}
+
+	List<Product> mergeSmart(const List<Description>& desc, const List<Price>& price) {
+		List<Product> priceList;
+		// TODO: Add your code here to build a list of products
+		//         using smart pointers
+
+
+
+
+
+
+		return priceList;
+	}
+}
